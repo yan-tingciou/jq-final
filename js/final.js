8a58f016084fc48eaf3bad09d28c08d5e8b44ff7
yan-tingciou <angelaavril04@gmail.com>
2023-05-01 17:25:57 +0800

    更新10

--- js/final.js
+++ js/final.js
@@ -1,38 +1,50 @@
+//頁面滑到700px左右時出現gotop
+function showBtnCondition() {
+  if ($(this).scrollTop() > 700) {
+    $('#gotop').fadeIn();
+      } else {
+       $('#gotop').fadeOut();
+     }
+   }
+$(window).scroll(showBtnCondition);
 $(document).ready(function(){
-    $('.list > li >a').click(function(event){
-      event.preventDefault();
-      $(this).parent().find('ul').slideToggle();
-      $(this).parent().siblings().find('ul').slideUp();
-      
-    });
-    const swiper = new Swiper('.swiper', {
-    // Optional parameters
-      direction: 'horizontal',
-      autoplay: {
-      delay: 3000,
-      },
-      loop: true,
-      effect: 'fade',
-      speed: 800,
-  
-      // If we need pagination
-      pagination: {
-        el: '.swiper-pagination',
-      },
-  
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-  
-      // And if we need scrollbar
-      scrollbar: {
-        el: '.swiper-scrollbar',
-      },
-    });
-      lightbox.option({
-      'resizeDuration': 1000,
-      'wrapAround': true
-    })
+ $('.list > li >a').click(function(event){
+   event.preventDefault();
+   $(this).parent().find('ul').slideToggle();
+   $(this).parent().siblings().find('ul').slideUp();
+   
+ });
+ const swiper = new Swiper('.swiper', {
+ // Optional parameters
+   direction: 'horizontal',
+   autoplay: {
+   delay: 3000,
+   },
+   loop: true,
+   effect: 'fade',
+   speed: 800,
+
+   // If we need pagination
+   pagination: {
+     el: '.swiper-pagination',
+   },
+
+   // Navigation arrows
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
+
+   // And if we need scrollbar
+   scrollbar: {
+     el: '.swiper-scrollbar',
+   },
+ });
+ 
+ //course的燈箱效果
+   lightbox.option({
+   'resizeDuration': 500,
+   'wrapAround': true
+   })
+ 
 });
